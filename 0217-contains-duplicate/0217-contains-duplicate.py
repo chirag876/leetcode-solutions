b570b9191a7af945ddea848ac942347c66ca84be
Chirag Gupta <108919262+chirag876@users.noreply.github.com>
2026-06-24 17:48:28 +0530

    Time: 67 ms (10.95%), Space: 26.5 MB (6.2%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        """
+    :type nums: List[int]
+    :rtype: bool
+    """
+        freq = {}
+        for i in range(len(nums)):
+            if nums[i] in freq:
+                freq[nums[i]] +=1
+            else:
+                freq[nums[i]] = 1
+        for keys in freq.keys():
+            if freq[keys] >=2:
+                return True
+        return False
+    
+        
